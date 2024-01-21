0b7a2f740014ceb56bf599dd931b0b2f0583a44e
Fadilix <iamfadile2@gmail.com>
2024-01-21 16:09:40 +0000

    better script again

--- public/js/inscription.js
+++ public/js/inscription.js
@@ -1,35 +1,31 @@
+const username = document.querySelector(".username");
 const password = document.querySelector(".password");
 const msg = document.querySelector(".msg");
-
-const username = document.querySelector(".username");
 const submit = document.querySelector(".submit");
 
 submit.disabled = true;
 
 function isUsernameValid() {
-    let motifRegex = /^[a-zA-Z0-9]+$/;
-    return motifRegex.test(username.value) && !/\s/.test(username.value);
+  const usernameRegex = /^[a-zA-Z0-9_]+$/;
+  return !username.value.includes(" ") || usernameRegex.test(username.value);
 }
 
 function isPasswordValid() {
-    return password.value.length >= 8;
+  return password.value.length >= 8;
 }
 
-function updateSubmitButton() {
-    submit.disabled = !(isUsernameValid() && isPasswordValid());
+function updateButtonState() {
+  if (isUsernameValid() && isPasswordValid()) {
+    submit.disabled = false;
+    msg.textContent = "";
+  } else if (!isUsernameValid()) {
+    submit.disabled = true;
+    msg.textContent = "Le nom d'utilisateur ne doit pas contenir d'espaces ni de caractères spéciaux.";
+  } else if (!isPasswordValid()) {
+    submit.disabled = true;
+    msg.textContent = "Le nombre de caractères du mot de passe doit être supérieur ou égal à 8.";
+  }
 }
 
-username.addEventListener("keyup", () => {
-    updateSubmitButton();
-});
-
-password.addEventListener("keyup", () => {
-    if (password.value.length < 8) {
-        msg.innerHTML = "Le nombre minimal pour un mot de passe est de 8 caractères";
-        msg.style.color = "red";
-    } else {
-        msg.innerHTML = "";
-    }
-
-    updateSubmitButton();
-});
+username.addEventListener("keyup", updateButtonState);
+password.addEventListener("keyup", updateButtonState);
