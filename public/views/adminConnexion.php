1b514b7b511840e529d9bf3ba549d6575058a8ab
Fadilix <iamfadile2@gmail.com>
2024-01-28 19:11:30 +0000

    added styles to the admin auth

--- public/views/adminConnexion.php
+++ public/views/adminConnexion.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../css/adminAuth.css">
     <?php
 
     include "../../config/database.php";
@@ -22,24 +23,36 @@
     ?>
 
 </head>
+<?php include "../components/adminAuthNav.php" ?>
 
 <body>
+    <h1>Connexion <span>administrateur</span></h1>
     <form action="" method="POST">
-        <div>
-            <label for="">Nom d'utilisateur</label>
-            <input type="text" placeholder="Entrer votre nom d'utilisateur" name="username" class="username">
+        <div class="image"></div>
+        <div class="form">
+            <div class="welcome">
+                <h2>Bienvenue</h2>
+                <p>Veuillez remplir tous les champs</p>
+            </div>
+            <div class="real-form">
+
+                <div>
+                    <label for="">Nom d'utilisateur</label>
+                    <input type="text" placeholder="Enter votre nom d'utilisateur" name="username">
+                </div>
+
+                <div>
+                    <label for="">Mot de passe</label>
+                    <input type="password" placeholder="Enter votre mot de passe" name="password" class="password">
+                </div>
+                <p class="msg" style="color: red">
+                    <?php echo $msg; ?>
+                </p>
+                <div class="button-wrapper">
+                    <button type="submit" name="submit">Se connecter</button>
+                </div>
+            </div>
         </div>
-
-        <div>
-            <label for="">Mot de passe</label>
-            <input type="password" placeholder="Entrer votre mot de passe" name="password" class="password">
-        </div>
-
-
-        <p class="msg">
-            <?php echo $msg; ?>
-        </p>
-        <button type="submit" class="submit">S'inscrire</button>
     </form>
 </body>
 
--- public/views/adminInscription.php
+++ public/views/adminInscription.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../css/adminAuth.css">
 </head>
 
 <body>
@@ -27,21 +28,34 @@
     }
 
     ?>
+    <?php include "../components/adminAuthNav.php" ?>
+    <h1>Inscription <span>administrateur</span></h1>
     <form action="" method="POST">
-        <div>
-            <label for="">Nom d'utilisateur</label>
-            <input type="text" placeholder="Entrer votre nom d'utilisateur" name="username" class="username">
+        <div class="image"></div>
+        <div class="form">
+            <div class="welcome">
+                <h2>Inscription</h2>
+                <p>Veuillez remplir tous les champs</p>
+            </div>
+            <div class="real-form">
+
+                <div>
+                    <label for="">Nom d'utilisateur</label>
+                    <input type="text" placeholder="Enter votre nom d'utilisateur" name="username">
+                </div>
+
+                <div>
+                    <label for="">Mot de passe</label>
+                    <input type="password" placeholder="Enter votre mot de passe" name="password" class="password">
+                </div>
+                <p class="msg" style="color: red">
+                    <?php echo $msg; ?>
+                </p>
+                <div class="button-wrapper">
+                    <button type="submit" name="submit">S'inscrire</button>
+                </div>
+            </div>
         </div>
-
-        <div>
-            <label for="">Mot de passe</label>
-            <input type="password" placeholder="Entrer votre mot de passe" name="password" class="password">
-        </div>
-
-
-        <p class="msg"><?php echo $msg ?></p>
-        <button type="submit" class="submit">S'inscrire</button>
-
     </form>
 
     <script src="../js/inscription.js"></script>
